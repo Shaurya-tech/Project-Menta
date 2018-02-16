3e538a1034dbfc7c6797708c535398e97696405d
ShauryaKumarTech <31845301+ShauryaKumarTech@users.noreply.github.com>
2018-02-16 18:47:00 +1100

    Add files via upload

---
+++ Project Menta/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Project_Menta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Resize = new System.Windows.Forms.Timer(this.components);
+            this.bunifuDragControl1 = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.GapPanel = new System.Windows.Forms.Panel();
+            this.BasePanel = new System.Windows.Forms.Panel();
+            this.SidePanel = new System.Windows.Forms.Panel();
+            this.BasePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Resize
+            // 
+            this.Resize.Tick += new System.EventHandler(this.Resize_Tick);
+            // 
+            // bunifuDragControl1
+            // 
+            this.bunifuDragControl1.Fixed = true;
+            this.bunifuDragControl1.Horizontal = true;
+            this.bunifuDragControl1.TargetControl = this.TopPanel;
+            this.bunifuDragControl1.Vertical = true;
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(51)))), ((int)(((byte)(53)))));
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(919, 32);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // GapPanel
+            // 
+            this.GapPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.GapPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GapPanel.Location = new System.Drawing.Point(0, 32);
+            this.GapPanel.Name = "GapPanel";
+            this.GapPanel.Size = new System.Drawing.Size(919, 24);
+            this.GapPanel.TabIndex = 1;
+            // 
+            // BasePanel
+            // 
+            this.BasePanel.Controls.Add(this.SidePanel);
+            this.BasePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BasePanel.Location = new System.Drawing.Point(0, 56);
+            this.BasePanel.Name = "BasePanel";
+            this.BasePanel.Size = new System.Drawing.Size(919, 461);
+            this.BasePanel.TabIndex = 2;
+            // 
+            // SidePanel
+            // 
+            this.SidePanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(63)))), ((int)(((byte)(104)))));
+            this.SidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SidePanel.Location = new System.Drawing.Point(0, 0);
+            this.SidePanel.Name = "SidePanel";
+            this.SidePanel.Size = new System.Drawing.Size(164, 461);
+            this.SidePanel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 517);
+            this.Controls.Add(this.BasePanel);
+            this.Controls.Add(this.GapPanel);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.ControlLight;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.BasePanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer Resize;
+        private Bunifu.Framework.UI.BunifuDragControl bunifuDragControl1;
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.Panel GapPanel;
+        private System.Windows.Forms.Panel BasePanel;
+        private System.Windows.Forms.Panel SidePanel;
+    }
+}
+
---
+++ Project Menta/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Menta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        protected override void OnPaint(PaintEventArgs e) // you can safely omit this method if you want
+        {
+           /* e.Graphics.FillRectangle(Brushes.Green, Top);
+            e.Graphics.FillRectangle(Brushes.Green, Left);
+            e.Graphics.FillRectangle(Brushes.Green, Right);
+            e.Graphics.FillRectangle(Brushes.Green, Bottom); */
+        }
+
+        private const int
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17;
+
+        const int _ = 4; // you can rename this variable if you like
+
+        Rectangle Top { get { return new Rectangle(0, 0, this.ClientSize.Width, _); } }
+        Rectangle Left { get { return new Rectangle(0, 0, _, this.ClientSize.Height); } }
+
+        private void Resize_Tick(object sender, EventArgs e)
+        {
+            // 4, 85, 216
+
+            int margin = 2;
+            this.BasePanel.Location = new Point(GapPanel.Left + margin, GapPanel.Top + GapPanel.Height);
+            this.BasePanel.Width = this.Width - (margin * 2);
+            this.BasePanel.Height = this.Height - (TopPanel.Height + GapPanel.Height + margin);
+        }
+
+        private void QuickSurvey()
+        {
+            int sFactor = 20;
+
+            ExtendedPanel surveyPanel = new ExtendedPanel();
+            surveyPanel.Width = 500;
+            surveyPanel.Height = 300;
+            surveyPanel.Location = new Point((BasePanel.Width / 2) - surveyPanel.Width / 2, BasePanel.Height / 2 - surveyPanel.Height / 2);
+            surveyPanel.BackColor = Color.Tomato;
+            surveyPanel.Opacity = 50;
+
+            int factor = -35;
+
+            Bunifu.Framework.UI.BunifuImageButton smile = new Bunifu.Framework.UI.BunifuImageButton();
+            smile.Image = Image.FromFile(Environment.CurrentDirectory + @"\Icons\happy-480.png");
+            smile.BackColor = Color.Transparent;
+            smile.SizeMode = PictureBoxSizeMode.Zoom;
+            smile.Zoom = 0;
+            smile.Width = 100;
+            smile.Height = 100;
+            smile.Location = new Point((surveyPanel.Width / 3) - (smile.Width - factor), surveyPanel.Top + 10);
+
+            Bunifu.Framework.UI.BunifuImageButton medium = new Bunifu.Framework.UI.BunifuImageButton();
+            medium.Image = Image.FromFile(Environment.CurrentDirectory + @"\Icons\happy-480.png");
+            medium.BackColor = Color.Transparent;
+            medium.SizeMode = PictureBoxSizeMode.Zoom;
+            medium.Zoom = 0;
+            medium.Width = 100;
+            medium.Height = 100;
+            medium.Location = new Point((surveyPanel.Width / 3)*2 - (medium.Width - factor), surveyPanel.Top + 10);
+
+            Bunifu.Framework.UI.BunifuImageButton sad = new Bunifu.Framework.UI.BunifuImageButton();
+            sad.Image = Image.FromFile(Environment.CurrentDirectory + @"\Icons\happy-480.png");
+            sad.BackColor = Color.Transparent;
+            sad.SizeMode = PictureBoxSizeMode.Zoom;
+            sad.Zoom = 0;
+            sad.Width = 100;
+            sad.Height = 100;
+            sad.Location = new Point((surveyPanel.Width / 3)*3 - (sad.Width - factor), surveyPanel.Top + 10);
+
+
+            surveyPanel.Controls.Add(smile);
+            surveyPanel.Controls.Add(medium);
+            surveyPanel.Controls.Add(sad);
+
+            BasePanel.Controls.Add(surveyPanel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BasePanel.Dock = DockStyle.None;
+            Resize.Start();
+            QuickSurvey();
+        }
+
+        Rectangle Bottom { get { return new Rectangle(0, this.ClientSize.Height - _, this.ClientSize.Width, _); } }
+        Rectangle Right { get { return new Rectangle(this.ClientSize.Width - _, 0, _, this.ClientSize.Height); } }
+
+        Rectangle TopLeft { get { return new Rectangle(0, 0, _, _); } }
+        Rectangle TopRight { get { return new Rectangle(this.ClientSize.Width - _, 0, _, _); } }
+        Rectangle BottomLeft { get { return new Rectangle(0, this.ClientSize.Height - _, _, _); } }
+        Rectangle BottomRight { get { return new Rectangle(this.ClientSize.Width - _, this.ClientSize.Height - _, _, _); } }
+
+
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == 0x84) // WM_NCHITTEST
+            {
+                var cursor = this.PointToClient(Cursor.Position);
+
+                if (TopLeft.Contains(cursor)) message.Result = (IntPtr)HTTOPLEFT;
+                else if (TopRight.Contains(cursor)) message.Result = (IntPtr)HTTOPRIGHT;
+                else if (BottomLeft.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (BottomRight.Contains(cursor)) message.Result = (IntPtr)HTBOTTOMRIGHT;
+
+                else if (Top.Contains(cursor)) message.Result = (IntPtr)HTTOP;
+                else if (Left.Contains(cursor)) message.Result = (IntPtr)HTLEFT;
+                else if (Right.Contains(cursor)) message.Result = (IntPtr)HTRIGHT;
+                else if (Bottom.Contains(cursor)) message.Result = (IntPtr)HTBOTTOM;
+            }
+        }
+    }
+}
